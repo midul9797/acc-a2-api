9feafcd5c5897522b692fb84a60cd292ecae46bc
midul9797 <mrmnso9797@gmail.com>
2022-09-28 02:14:22 +0600

    don

--- utils/DBConnect.js
+++ utils/DBConnect.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose");
 require("dotenv").config();
 const DBConnect = () =>{
-    mongoose.connect(process.env.DATABASE)
+    mongoose.connect(`mongodb+srv://acc-a2:${process.env.DATABASE}@cluster0.worr8fb.mongodb.net/?retryWrites=true&w=majority`)
         .then(()=>{
             console.log("Database connected");
         })
 }
 module.exports = DBConnect;
+// 5Zryt1SfjvAcVTKe
